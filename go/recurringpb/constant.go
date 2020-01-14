da02b724903d2c7e3a2cb53b19b8568ec9be2003
Vadim Sabirov <pr0head@gmail.com>
2020-01-15 02:50:09 +0300

    refactor: Add constants for the recurringpb service

---
+++ go/recurringpb/constant.go
@@ -0,0 +1,14 @@
+package recurringpb
+
+const (
+	PayOneRepositoryServiceName = "p1payrepository"
+	PayOneMicroserviceVersion   = "latest"
+
+	OrderPublicStatusCreated    = "created"
+	OrderPublicStatusProcessed  = "processed"
+	OrderPublicStatusCanceled   = "canceled"
+	OrderPublicStatusRejected   = "rejected"
+	OrderPublicStatusRefunded   = "refunded"
+	OrderPublicStatusChargeback = "chargeback"
+	OrderPublicStatusPending    = "pending"
+)
