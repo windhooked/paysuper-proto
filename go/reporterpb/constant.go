c849b863407cc4b5d1e0bcf99d0fcf57a636d0d3
Vadim Sabirov <pr0head@gmail.com>
2020-01-17 20:15:14 +0300

    refactor: Add constants to the reporter service

--- go/reporterpb/constant.go
+++ go/reporterpb/constant.go
@@ -3,4 +3,33 @@ package reporterpb
 const (
 	ServiceName    = "p1payreporter"
 	ServiceVersion = "latest"
+
+	ReportTypeTransactions        = "transactions"
+	ReportTypeVat                 = "vat"
+	ReportTypeVatTransactions     = "vat_transactions"
+	ReportTypeRoyalty             = "royalty"
+	ReportTypeRoyaltyTransactions = "royalty_transactions"
+	ReportTypePayout              = "payout"
+	ReportTypeAgreement           = "agreement"
+
+	OutputExtensionXlsx = "xlsx"
+	OutputExtensionCsv  = "csv"
+	OutputExtensionPdf  = "pdf"
+
+	RequestParameterAgreementNumber                             = "number"
+	RequestParameterAgreementLegalName                          = "legal_name"
+	RequestParameterAgreementAddress                            = "address"
+	RequestParameterAgreementRegistrationNumber                 = "registration_number"
+	RequestParameterAgreementPayoutCost                         = "payout_cost"
+	RequestParameterAgreementMinimalPayoutLimit                 = "minimal_payout_limit"
+	RequestParameterAgreementPayoutCurrency                     = "payout_currency"
+	RequestParameterAgreementPSRate                             = "ps_rate"
+	RequestParameterAgreementHomeRegion                         = "home_region"
+	RequestParameterAgreementMerchantAuthorizedName             = "merchant_authorized_name"
+	RequestParameterAgreementMerchantAuthorizedPosition         = "merchant_authorized_position"
+	RequestParameterAgreementOperatingCompanyLegalName          = "oc_name"
+	RequestParameterAgreementOperatingCompanyAddress            = "oc_address"
+	RequestParameterAgreementOperatingCompanyRegistrationNumber = "oc_registration_number"
+	RequestParameterAgreementOperatingCompanyAuthorizedName     = "oc_authorized_name"
+	RequestParameterAgreementOperatingCompanyAuthorizedPosition = "oc_authorized_position"
 )
