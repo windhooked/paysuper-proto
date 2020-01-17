589d168b9a8f439196331389bae924fdc58f418a
Vadim Sabirov <pr0head@gmail.com>
2020-01-17 16:57:10 +0300

    refactor: Add constants to the document signer service

--- go/document_signerpb/constant.go
+++ go/document_signerpb/constant.go
@@ -3,4 +3,13 @@ package document_signerpb
 const (
 	ServiceName    = "psdocumentsigner"
 	ServiceVersion = "latest"
+
+	SignerRoleNameMerchant = "Merchant"
+	SignerRoleNamePaysuper = "Paysuper"
+
+	MetadataFieldMerchantId       = "merchant_id"
+	MetadataFieldPayoutDocumentId = "payout_document_id"
+
+	StorageTypeAgreement = "agreement"
+	StorageTypeReport    = "report"
 )
