250d55540ab1a35cfc56079b656f98ab0b208c89
Vadim Sabirov <pr0head@gmail.com>
2020-01-19 02:44:50 +0300

    refactor: Add constants to the recurring service

--- go/recurringpb/constant.go
+++ go/recurringpb/constant.go
@@ -11,4 +11,27 @@ const (
 	OrderPublicStatusRefunded   = "refunded"
 	OrderPublicStatusChargeback = "chargeback"
 	OrderPublicStatusPending    = "pending"
+
+	OrderStatusNew                         = 0
+	OrderStatusPaymentSystemCreate         = 1
+	OrderStatusPaymentSystemRejectOnCreate = 2
+	OrderStatusPaymentSystemReject         = 3
+	OrderStatusPaymentSystemComplete       = 4
+	OrderStatusProjectInProgress           = 5
+	OrderStatusProjectComplete             = 6
+	OrderStatusProjectPending              = 7
+	OrderStatusProjectReject               = 8
+	OrderStatusRefund                      = 9
+	OrderStatusChargeback                  = 10
+	OrderStatusPaymentSystemDeclined       = 11
+	OrderStatusPaymentSystemCanceled       = 12
+	OrderStatusItemReplaced                = 13
+
+	PaymentSystemCardPayDateFormat  = "2006-01-02T15:04:05Z"
+	PaymentSystemGroupAliasBankCard = "BANKCARD"
+	PaymentSystemGroupAliasWebMoney = "WEBMONEY"
+	PaymentSystemGroupAliasQiwi     = "QIWI"
+	PaymentSystemGroupAliasNeteller = "NETELLER"
+	PaymentSystemGroupAliasAlipay   = "ALIPAY"
+	PaymentSystemGroupAliasBitcoin  = "BITCOIN"
 )
