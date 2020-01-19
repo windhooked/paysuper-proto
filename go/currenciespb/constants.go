6af7d20968544e393655b3f0f76100e65a7d0fbc
Vadim Sabirov <pr0head@gmail.com>
2020-01-19 03:21:05 +0300

    refactor: Add constants to the currency service

--- go/currenciespb/constants.go
+++ go/currenciespb/constants.go
@@ -6,4 +6,19 @@ const (
 
 	// Version - version of service
 	Version = "latest"
+
+	// RateTypeOxr - rate type value for Oxr rates
+	RateTypeOxr = "oxr"
+
+	// RateTypeCentralbanks - rate type value for central banks rates
+	RateTypeCentralbanks = "centralbanks"
+
+	// RateTypePaysuper - rate type value for Paysuper rates
+	RateTypePaysuper = "paysuper"
+
+	// RateTypeStock - rate type value for Stock rates
+	RateTypeStock = "stock"
+
+	// RateTypeCardpay - rate type value for Cardpay rates
+	RateTypeCardpay = "cardpay"
 )
