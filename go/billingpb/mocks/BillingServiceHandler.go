dda4b43575acce40decc52c2bee3d1b283e4c7bd
Travis CI <Travis CI>
2020-02-05 18:07:32 +0000

    Update repo [skip ci]

--- go/billingpb/mocks/BillingServiceHandler.go
+++ go/billingpb/mocks/BillingServiceHandler.go
@@ -1943,6 +1943,20 @@ func (_m *BillingServiceHandler) MerchantReviewRoyaltyReport(_a0 context.Context
 	return r0
 }
 
+// NotifyWebhookTestResults provides a mock function with given fields: _a0, _a1, _a2
+func (_m *BillingServiceHandler) NotifyWebhookTestResults(_a0 context.Context, _a1 *billingpb.NotifyWebhookTestResultsRequest, _a2 *billingpb.EmptyResponseWithStatus) error {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.NotifyWebhookTestResultsRequest, *billingpb.EmptyResponseWithStatus) error); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // OrderCreateByPaylink provides a mock function with given fields: _a0, _a1, _a2
 func (_m *BillingServiceHandler) OrderCreateByPaylink(_a0 context.Context, _a1 *billingpb.OrderCreateByPaylink, _a2 *billingpb.OrderCreateProcessResponse) error {
 	ret := _m.Called(_a0, _a1, _a2)
@@ -2201,6 +2215,20 @@ func (_m *BillingServiceHandler) RoyaltyReportPdfUploaded(_a0 context.Context, _
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.RoyaltyReportPdfUploadedRequest, *billingpb.RoyaltyReportPdfUploadedResponse) error); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// SendWebhookToMerchant provides a mock function with given fields: _a0, _a1, _a2
+func (_m *BillingServiceHandler) SendWebhookToMerchant(_a0 context.Context, _a1 *billingpb.OrderCreateRequest, _a2 *billingpb.SendWebhookToMerchantResponse) error {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.OrderCreateRequest, *billingpb.SendWebhookToMerchantResponse) error); ok {
 		r0 = rf(_a0, _a1, _a2)
 	} else {
 		r0 = ret.Error(0)
