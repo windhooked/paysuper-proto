adf09ee279b0f5bfef45b173b6a5073f5e1cdda9
Travis CI <Travis CI>
2020-08-31 10:57:25 +0000

    Update repo [skip ci]

--- go/billingpb/mocks/BillingServiceHandler.go
+++ go/billingpb/mocks/BillingServiceHandler.go
@@ -743,20 +743,6 @@ func (_m *BillingServiceHandler) FinishRedeemKeyForOrder(_a0 context.Context, _a
 	return r0
 }
 
-// GetActOfCompletion provides a mock function with given fields: _a0, _a1, _a2
-func (_m *BillingServiceHandler) GetActOfCompletion(_a0 context.Context, _a1 *billingpb.ActOfCompletionRequest, _a2 *billingpb.ActOfCompletionResponse) error {
-	ret := _m.Called(_a0, _a1, _a2)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.ActOfCompletionRequest, *billingpb.ActOfCompletionResponse) error); ok {
-		r0 = rf(_a0, _a1, _a2)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
 // GetAdminByUserId provides a mock function with given fields: _a0, _a1, _a2
 func (_m *BillingServiceHandler) GetAdminByUserId(_a0 context.Context, _a1 *billingpb.CommonUserProfileRequest, _a2 *billingpb.UserRoleResponse) error {
 	ret := _m.Called(_a0, _a1, _a2)
@@ -2289,6 +2275,20 @@ func (_m *BillingServiceHandler) SendWebhookToMerchant(_a0 context.Context, _a1 
 
 	var r0 error
 	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.OrderCreateRequest, *billingpb.SendWebhookToMerchantResponse) error); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// SetCustomerPaymentActivity provides a mock function with given fields: _a0, _a1, _a2
+func (_m *BillingServiceHandler) SetCustomerPaymentActivity(_a0 context.Context, _a1 *billingpb.SetCustomerPaymentActivityRequest, _a2 *billingpb.EmptyResponseWithStatus) error {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.SetCustomerPaymentActivityRequest, *billingpb.EmptyResponseWithStatus) error); ok {
 		r0 = rf(_a0, _a1, _a2)
 	} else {
 		r0 = ret.Error(0)
