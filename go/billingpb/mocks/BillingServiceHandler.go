12dcedb8bb16e9f6f815e1ece587a0782da20a76
Travis CI <Travis CI>
2020-09-23 21:55:09 +0000

    Update repo [skip ci]

--- go/billingpb/mocks/BillingServiceHandler.go
+++ go/billingpb/mocks/BillingServiceHandler.go
@@ -505,6 +505,20 @@ func (_m *BillingServiceHandler) DeleteAdminUser(_a0 context.Context, _a1 *billi
 	return r0
 }
 
+// DeleteCustomerCard provides a mock function with given fields: _a0, _a1, _a2
+func (_m *BillingServiceHandler) DeleteCustomerCard(_a0 context.Context, _a1 *billingpb.DeleteCustomerCardRequest, _a2 *billingpb.DeleteCustomerCardResponse) error {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.DeleteCustomerCardRequest, *billingpb.DeleteCustomerCardResponse) error); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // DeleteKeyProduct provides a mock function with given fields: _a0, _a1, _a2
 func (_m *BillingServiceHandler) DeleteKeyProduct(_a0 context.Context, _a1 *billingpb.RequestKeyProductMerchant, _a2 *billingpb.EmptyResponseWithStatus) error {
 	ret := _m.Called(_a0, _a1, _a2)
