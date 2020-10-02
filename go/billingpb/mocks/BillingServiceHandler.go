7bf7b76c2a5278cf0e63f93e84c4b7914ab01f6d
Travis CI <Travis CI>
2020-10-02 15:03:31 +0000

    Update repo [skip ci]

--- go/billingpb/mocks/BillingServiceHandler.go
+++ go/billingpb/mocks/BillingServiceHandler.go
@@ -1009,6 +1009,20 @@ func (_m *BillingServiceHandler) GetCustomerList(_a0 context.Context, _a1 *billi
 	return r0
 }
 
+// GetCustomerShortInfo provides a mock function with given fields: _a0, _a1, _a2
+func (_m *BillingServiceHandler) GetCustomerShortInfo(_a0 context.Context, _a1 *billingpb.GetCustomerShortInfoRequest, _a2 *billingpb.GetCustomerShortInfoResponse) error {
+	ret := _m.Called(_a0, _a1, _a2)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *billingpb.GetCustomerShortInfoRequest, *billingpb.GetCustomerShortInfoResponse) error); ok {
+		r0 = rf(_a0, _a1, _a2)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // GetCustomerSubscription provides a mock function with given fields: _a0, _a1, _a2
 func (_m *BillingServiceHandler) GetCustomerSubscription(_a0 context.Context, _a1 *billingpb.GetSubscriptionRequest, _a2 *billingpb.GetSubscriptionResponse) error {
 	ret := _m.Called(_a0, _a1, _a2)
